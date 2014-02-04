1431c2711f9653feef0f1a43a097bff60c059556
tbsmithFS <tbsmith@fullsail.com>
2014-02-03 22:23:46 -0500

    Further development

---
+++ Assets/FishSwarm.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishSwarm : MonoBehaviour {
+
+	public int numberInSwarm = 10;
+	ArrayList fishSwarm = new ArrayList(numberInSwarm);
+	GameObject fish;
+
+	// Use this for initialization
+	void Start () {
+
+		fish = GameObject.FindGameObjectWithTag("Fish");
+
+		for (int i=0; i<numberInSwarm; i++) {
+			Debug.Log("i = " + i);
+			Rigidbody newFish;
+			newFish = Instantiate(fish) as Rigidbody;
+			newFish.position = Random.insideUnitSphere * 10;
+			fishSwarm.Add(newFish);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
