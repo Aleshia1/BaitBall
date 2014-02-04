90a8fc6510109b9b3c89f3c3f0286070c2ecf358
Todd Smith <elliptic1@gmail.com>
2014-02-03 21:34:37 -0500

    First commit

---
+++ Assets/Fish.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fish : MonoBehaviour {
+
+	public GameObject dolphin;
+	public float distanceForceMultiplier;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = Vector3.Distance(dolphin.transform.position, this.transform.position);
+		if (distance < 3) {
+			rigidbody.AddForce(
+				distanceForceMultiplier * 
+				Vector3.Normalize(
+					this.transform.position - dolphin.transform.position
+				)
+				);
+		}
+	}
+}
